a69357aa77f8658da95b94335f9623cffa8fa1a3
wangsen <1162103564@qq.com>
2019-10-28 19:17:14 +0800

    再提交一次

---
+++ src/main/java/cn/kgc/domain/a.java
@@ -0,0 +1,12 @@
+package cn.kgc.domain;
+
+public class a {
+
+    public String a;
+
+    public static void main(String[] args) {
+       String a="dididi";
+
+        System.out.println(a);
+    }
+}
